50b797ced7f32cef1a46b11084873c3485b55d01
Rich Hildred <rhildred@gmail.com>
2014-10-14 21:17:24 -0400

    looks like I missed something

--- www/js/lib/jquery.nav.js
+++ www/js/lib/jquery.nav.js
@@ -7,32 +7,41 @@ function setActive(page) {
     }
 }
 
-define(["jquery", "text!../../nav.html", "history"], function (jQuery, sNav) {
+define(["jquery", "text!../../nav.html", "underscore", "history"], function (jQuery, sNav, _) {
     jQuery.fn.nav = function () {
         var path = window.location.pathname;
         var page = path.split("/").pop();
-        this.html(sNav);
+        var fTemplate = _.template(sNav);
+        this.html(fTemplate({}));
         setActive(page);
+
 
         // we are clicking on navbar and want to set the history
         jQuery(".navbar a:not(.dropdown-toggle)").click(function (evt) {
             var sPage = evt.currentTarget.href.split("/").pop();
-            var sTitle = evt.currentTarget.title;
             if (sPage == "index.html" || sPage == "")
                 sPage = ".";
             History.pushState({
                 state: sPage
-            }, sTitle, sPage);
+            }, "", sPage);
             return false;
         });
 
+        // we need to know if we are in a replace state or a push state
+        var bInBind = false;
         // history has changed either as a result of clicking or the back button
         History.Adapter.bind(window, 'statechange', function () { // Note: We are using statechange instead of popstate
+            if(bInBind)return;
+            bInBind = true;
             var State = History.getState(); // Note: We are using History.getState() instead of event.state
             var sPage = State.cleanUrl.split("/").pop();
             setActive(sPage);
             var sUrl = sPage + " #content";
-            jQuery("#content").load(sUrl);
+            jQuery("#content").load(sUrl, function(sHtml){
+                var sTitle = sHtml.match(/<title[^>]*>([^<]+)<\/title>/)[1];
+                History.replaceState({state:sPage}, sTitle, sPage);
+                bInBind = false;
+            });
         });
 
         return this;
