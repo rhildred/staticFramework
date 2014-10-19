63a0533ede6e172dc0a168b4156af9cf4d57bf37
Rich Hildred <rhildred@gmail.com>
2014-10-19 09:02:09 -0400

    added bootstrap shim that seemed to help my other pages to work

--- www/js/app.js
+++ www/js/app.js
@@ -5,11 +5,61 @@
 requirejs.config({
     "baseUrl": "js/lib",
     "paths": {
-      "app": "../app",
-      "jquery": "//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min",
+        "app": "../app",
+        "jquery": "//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min",
         "bootstrap": "//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min",
         "history": "//cdnjs.cloudflare.com/ajax/libs/history.js/1.8/bundled-uncompressed/html4+html5/jquery.history",
         "underscore": "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.7.0/underscore-min"
+    },
+    shim: {
+        'bootstrap/affix': {
+            deps: ['jquery'],
+            exports: '$.fn.affix'
+        },
+        'bootstrap/alert': {
+            deps: ['jquery'],
+            exports: '$.fn.alert'
+        },
+        'bootstrap/button': {
+            deps: ['jquery'],
+            exports: '$.fn.button'
+        },
+        'bootstrap/carousel': {
+            deps: ['jquery'],
+            exports: '$.fn.carousel'
+        },
+        'bootstrap/collapse': {
+            deps: ['jquery'],
+            exports: '$.fn.collapse'
+        },
+        'bootstrap/dropdown': {
+            deps: ['jquery'],
+            exports: '$.fn.dropdown'
+        },
+        'bootstrap/modal': {
+            deps: ['jquery'],
+            exports: '$.fn.modal'
+        },
+        'bootstrap/popover': {
+            deps: ['jquery'],
+            exports: '$.fn.popover'
+        },
+        'bootstrap/scrollspy': {
+            deps: ['jquery'],
+            exports: '$.fn.scrollspy'
+        },
+        'bootstrap/tab': {
+            deps: ['jquery'],
+            exports: '$.fn.tab'
+        },
+        'bootstrap/tooltip': {
+            deps: ['jquery'],
+            exports: '$.fn.tooltip'
+        },
+        'bootstrap/transition': {
+            deps: ['jquery'],
+            exports: '$.fn.transition'
+        }
     }
 });
 
