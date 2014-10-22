1791ef50d42b16c94f38d17f82b2d859e4b4ac5f
Richard Hildred <rhildred@gmail.com>
2014-10-22 18:35:02 -0400

    put in ajaxSetup

--- www/js/app/main.js
+++ www/js/app/main.js
@@ -1,6 +1,7 @@
 define(["jquery", "jquery.footer", "jquery.nav", "bootstrap"], function() {
     //the jquery.nav.js and jquery.footer.js plugins have been loaded.
     jQuery(function() {
+        jQuery.ajaxSetup({crossDomain: true, xhrFields: { withCredentials: true } });
         jQuery("#nav").nav();
         jQuery('#footer').footer();
         // we set this hidden in the CSS so that the page refresh can now be from top to bottom
