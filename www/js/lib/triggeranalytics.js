c1dc618d5a0c3c31873cf014dc32d8a5963e86ac
Rich Hildred <rhildred@gmail.com>
2014-11-20 21:33:37 -0500

    made a little fix to triggerAnalytics and remved analytics.js from the app.js

--- www/js/lib/triggeranalytics.js
+++ www/js/lib/triggeranalytics.js
@@ -1,8 +1,9 @@
-define(['ga'], function () {
+define(['https://www.google-analytics.com/analytics.js'], function () {
     // analytics number goes here
     window.ga('create', 'UA-45546538-1');
     return function () {
         //this gets done everytime we load a new page
+        window.ga('set', 'location', window.location);
         window.ga('send', 'pageview');
     };
 });
